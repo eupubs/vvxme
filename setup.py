4ddde8077868e91c0f53801859fce3a4a93360c5
eupubs <eupub@yahoo.com>
2019-10-07 17:08:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,12 +47,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',  # Required
+    version='1.0.0.post1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Manage your Poly VVX (CLI Menu available)',  # Optional
+    description='Manages your Poly VVX powered by RESTAPIs based on (UCS>=6.1), with CLI Menu or simply use the available VVX class to develope your own applications.',  # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
